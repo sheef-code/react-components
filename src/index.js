ac2eb278c127f10e922a5b5eb91b1c56c49a6040
sheef-code <shifathsalam@gmail.com>
2019-05-16 13:29:55 -0700

    worked on CommentDetail and ApprovalCard components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        content="Nice blog post"
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 2:00PM"
+        content="I like the subject"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 1:45PM"
+        content="I like the topic"
+      />
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
