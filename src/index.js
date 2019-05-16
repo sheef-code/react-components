3e9d653f0a8354268730e8245705c92b9fbd25db
sheef-code <shifathsalam@gmail.com>
2019-05-16 16:16:50 -0700

    Passed CommentDetail down to ApprovalCard

--- src/index.js
+++ src/index.js
@@ -6,22 +6,34 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content="Nice blog post"
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 2:00PM"
-        content="I like the subject"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 1:45PM"
-        content="I like the topic"
-      />
+      <br />
+      <ApprovalCard>
+        <div>
+          <h4>WARNING!</h4>
+        </div>
+        Are you sure you want to do this?
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 2:00PM"
+          content="I like the subject"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 1:45PM"
+          content="I like the topic"
+        />
+      </ApprovalCard>
     </div>
   );
 };
